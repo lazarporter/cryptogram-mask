2066552cb42e7d886830bb209f977b196eedee92
Lazer Porter <lazar.porter@gmail.com>
2021-12-24 00:42:22 -0500

    add cryptogram functionality

--- src/components/CryptogramMaker.tsx
+++ src/components/CryptogramMaker.tsx
@@ -1,6 +1,54 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  SxProps,
+  TextField,
+  Theme,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { makeCryptogram, makeKey } from "../util/cryptmogrify";
+
+const layout: SxProps<Theme> = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-around",
+  flexGrow: 1,
+  alignItems: "center",
+  padding: 3,
+};
 
 export const CryptogramMaker = () => {
-  return <Box>cryp</Box>;
+  const [input, setInput] = React.useState("");
+  const [key] = React.useState(() => makeKey());
+  const cryptogramOutput = React.useMemo(
+    () => makeCryptogram(input, key),
+    [input, key]
+  );
+
+  return (
+    <Box sx={layout}>
+      <TextField
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        multiline
+        placeholder="Enter some text to be cryptmogrified"
+        sx={{ width: "80%", maxWidth: "500px" }}
+      />
+      <Card sx={{ width: "80%", maxWidth: "500px" }}>
+        <CardHeader title="Output" />
+        <CardContent>
+          <Typography>{cryptogramOutput}</Typography>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardContent>
+          <Typography variant="h5">Key</Typography>
+          <Typography>{JSON.stringify(key, null, 4)}</Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 };
---
+++ src/util/cryptmogrify.ts
@@ -0,0 +1,61 @@
+// https://stackoverflow.com/a/2450976/10481855
+const shuffle = (array: string[]) => {
+  let currentIndex: number = array.length;
+  let randomIndex: number = 0;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+
+export const makeKey = (): { [key: string]: string }[] => {
+  const alphabet = Array.from(Array(26).keys()).map((num) =>
+    String.fromCharCode(num + 97)
+  );
+
+  const shuffledAlphabet = shuffle([...alphabet]);
+  const key = alphabet.map((char, i) => ({ [char]: shuffledAlphabet[i] }));
+  return key;
+};
+
+export const makeCryptogram = (
+  input: string,
+  key: { [key: string]: string }[]
+): string => {
+  let output = "";
+  for (let i = 0; i < input.length; i++) {
+    const currentChar = input[i];
+    if (currentChar >= "A" && currentChar <= "Z") {
+      const currentLetterKey = key.find(
+        (singleLetterKey) =>
+          Object.keys(singleLetterKey)[0] === currentChar.toLowerCase()
+      );
+
+      if (!currentLetterKey) return "Something went wrong...";
+      const cryptedChar = Object.values(currentLetterKey)[0];
+      output += cryptedChar.toUpperCase();
+    } else if (currentChar >= "a" && currentChar <= "z") {
+      const currentLetterKey = key.find(
+        (singleLetterKey) => Object.keys(singleLetterKey)[0] === currentChar
+      );
+      if (!currentLetterKey) return "Something went wrong...";
+      const cryptedChar = Object.values(currentLetterKey)[0];
+      output += cryptedChar;
+    } else {
+      // not a letter
+      output += currentChar;
+    }
+  }
+  return output;
+};
