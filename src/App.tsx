e07ee388a9fd6a529d6b16266a4ff04b796ceb9b
Lazer Porter <lazar.porter@gmail.com>
2021-12-23 23:22:59 -0500

    basic layout for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { Box } from "@mui/system";
-import { Typography } from "@mui/material";
+import { Box, SxProps } from "@mui/system";
+import { AppBar, Typography } from "@mui/material";
+import { Theme } from "@mui/material/styles";
 import { AppThemeProvider } from "./providers/ThemeProvider";
+import { CryptogramMaker } from "./components/CryptogramMaker";
+
+const appLayout: SxProps = {
+  outline: "3px dashed purple",
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+};
+
+const appBarStyles: SxProps = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  width: "100%",
+  outline: "3px dashed red",
+};
 
 export const App = () => {
   return (
     <AppThemeProvider>
-      <Box sx={{ backgroundColor: "primary.main" }}>
-        <Typography>Stuff goes here</Typography>
+      <Box sx={appLayout}>
+        <AppBar position="sticky" sx={appBarStyles}>
+          <Typography variant="h3">Cryptogram Generator</Typography>
+        </AppBar>
+        <CryptogramMaker />
       </Box>
     </AppThemeProvider>
   );
--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React from "react";
 
+
 const theme = createTheme({
   palette: {
     primary: {
